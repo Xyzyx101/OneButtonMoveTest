76aadad798c6232b006873e7aed8692e577a7372
Andrew Perrault <andrew.perrault@yahoo.ca>
2016-05-12 07:04:30 -0400

    Basically works.

--- Assets/Scripts/OneButtonMover.cs
+++ Assets/Scripts/OneButtonMover.cs
@@ -1,57 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
-using UnityStandardAssets.Characters.FirstPerson;
 
-public class OneButtonMover : MonoBehaviour {
+public class OneButtonMover : MonoBehaviour
+{
+    public float MoveSpeed = 1f;
+    public Vector2 LookSensitivity = new Vector2(1f, 1f);
+    public float Skew = 2.71828f;
+    public float Boost = 30f;
 
-	public Quaternion HeadDir;
-	public Quaternion BodyDir;
+    public Transform CameraTransform;
+    public Transform NeckTransform;
+    public Transform BodyTransform;
 
-	public Vector2 HeadMoveSpeed = new Vector2(1f, 1f);
-	public GameObject _CameraObject;
-	public Vector2 LookSensitivity = new Vector2(1,1);
+    private bool _MoveInput;
+    private Quaternion _NewCameraRot;
+    private Quaternion _NewNeckRot;
+    private Quaternion _NewBodyRot;
 
-	private Vector2 _AimInput;
-	private bool _MoveInput;
-	private Camera _Camera;
-	private Quaternion _CameraRot;
+    private Rigidbody RB;
 
-	void Start () {
-		_Camera = _CameraObject.GetComponent<Camera>();
-		_CameraRot = _CameraObject.transform.rotation;
-		HeadDir = transform.rotation;
-		BodyDir = transform.rotation;
-	}
+    void Start()
+    {
+        //CameraTransform = GetComponentInChildren<Camera>().transform;
+        //NeckTransform = CameraTransform.parent;
+        //BodyTransform = transform;
+        RB = GetComponent<Rigidbody>();
 
-	void Update () {
-		Debug.DrawRay(transform.position + Vector3.up * 1f, HeadDir * Vector3.forward * 2f, Color.yellow);
-		Debug.DrawRay(transform.position + Vector3.up * 1f, BodyDir * Vector3.forward * 2f, Color.blue);
-		//Vector3 eulerAim = new Vector3(_AimInput.y * HeadMoveSpeed.y, _AimInput.x * HeadMoveSpeed.x, 0) * Time.deltaTime;
-		//Quaternion newCamRot = Quaternion.Euler(eulerAim);
-		_Camera.transform.rotation = _CameraRot;
-	}
+        _NewCameraRot = CameraTransform.rotation;
+        _NewNeckRot = NeckTransform.rotation;
+        _NewBodyRot = BodyTransform.rotation;
+    }
 
-	void FixedUpdate () {
-		GetInput();
-	}
+    void Update()
+    {
+        Debug.DrawRay(transform.position + Vector3.up * 1f, CameraTransform.forward * 2f, Color.yellow);
+        Debug.DrawRay(transform.position + Vector3.up * 1f, NeckTransform.forward * 2f, Color.green);
+        Debug.DrawRay(transform.position + Vector3.up * 1f, BodyTransform.forward * 2f, Color.blue);
+        GetInput();
+        UpdateCamLook();
+        UpdateBodyHeading();
+        if (_MoveInput)
+        {
+            transform.position += BodyTransform.forward * MoveSpeed * Time.deltaTime;
+        }
+    }
 
-	private void GetInput()
-	{
-		float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-		float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+    private void GetInput()
+    {
+        float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * LookSensitivity.y;
+        float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * LookSensitivity.x;
+        _NewCameraRot *= Quaternion.Euler(-xRot, 0f, 0f);
+        _NewNeckRot *= Quaternion.Euler(0f, yRot, 0f);
+        _MoveInput = Input.GetMouseButton(0);
+    }
 
-		float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * LookSensitivity.y;
-		float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * LookSensitivity.x;
-		_CameraRot *= Quaternion.Euler (-xRot, yRot, 0f);
-		Debug.Log(_CameraRot);
+    private void UpdateCamLook()
+    {
+        CameraTransform.localRotation = _NewCameraRot;
+        NeckTransform.localRotation = _NewNeckRot;
+    }
 
-		_AimInput = new Vector2(horizontal, vertical);
-		_MoveInput = Input.GetKey(KeyCode.Space);
+    private void UpdateBodyHeading()
+    {
+        Vector3 perpVector = Vector3.Cross(Vector3.up, BodyTransform.forward);
+        float angle = Vector3.Angle(NeckTransform.forward, BodyTransform.forward);
+        angle *= Mathf.Sign(Vector3.Dot(perpVector, NeckTransform.forward));
 
-	}
+        float debug1 = angle;
 
-	private void UpdateCamLook() {
-		
-	}
+        angle = angle / 360f + 0.5f;
+        float outAngle = Mathf.Log(angle / (1f - angle), Skew) * Boost;
+
+        //Debug.Log("Angle : " + debug1 + "  Output : " + outAngle);
+        _NewBodyRot *= Quaternion.Euler(0f, outAngle, 0f);
+        BodyTransform.rotation = _NewBodyRot;
+    }
 }
